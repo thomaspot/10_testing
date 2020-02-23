80eb21ba891f68e48255be22498de8794179b22e
Thomas Pot <thomaspot@users.noreply.github.com>
2020-02-23 15:55:43 +0100

    Add files via upload

---
+++ ehcs_qr_code_invoice/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'QR Code Invoice',
+    'version': '10.0.1.0.0',
+    'category': 'Accountinh',
+    'author': 'ERP Harbor Consulting Services',
+    'summary': 'Generate QR Code for Invoice',
+    'website': 'http://www.erpharbor.com',
+    'description': """""",
+    'depends': [
+        'ehcs_qr_code_base',
+        'account',
+    ],
+    'data': [
+        'report/account_invoice_report_template.xml',
+        'views/qr_code_invoice_view.xml',
+    ],
+    'images': [
+        'static/description/banner.png',
+    ],
+}
